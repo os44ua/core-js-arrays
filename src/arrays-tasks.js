c3dcb94541839b4605a0806fda3764f0d45f3ac4
Olga <olga.slepova87@gmail.com>
2025-11-12 18:19:26 +0100

    feat: implement function getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -125,8 +125,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const average = arr.reduce((acc, x) => acc + x, 0) / arr.length;
+  return Math.round(average * 100) / 100;
 }
 
 /**
