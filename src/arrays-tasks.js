0872c51cc102ca2aa1c4558728f73a3193b735c8
Olga <olga.slepova87@gmail.com>
2025-11-13 15:20:33 +0100

    feat: implement funcion findLongestIncreasingSubsequence

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -501,8 +501,19 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => longest is [3, 10] and [1, 20] => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => longest is [7, 40, 80] => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  if (nums.length === 0) return 0;
+
+  const { maxLen } = nums.slice(1).reduce(
+    (acc, val, i) => {
+      const prev = nums[i];
+      const cur = val > prev ? acc.cur + 1 : 1;
+      return { cur, maxLen: Math.max(acc.maxLen, cur) };
+    },
+    { cur: 1, maxLen: 1 }
+  );
+
+  return maxLen;
 }
 
 /**
